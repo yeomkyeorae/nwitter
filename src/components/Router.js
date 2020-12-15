1ea09fa99b4a3a9b5790ade2c792dfcea98e59ad
yeomkyeorae <duarufp06@naver.com>
2020-12-15 15:09:47 +0900

    logout

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import Auth from '../routes/Auth';
-import Home from '../routes/Home';
+import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+import Navigation from "./Navigation";
+import Profile from "routes/Profile";
 
-const AppRouter = ({isLoggedIn}) => {
-
-    return(
-      <Router>
-        <Switch>
-          {isLoggedIn? (
-            <>
-              <Route exact path='/'>
-                <Home />
-              </Route>
-            </>) : (
-              <Route exact path='/'>
-                <Auth />
-              </Route>
-            )}
-        </Switch>
-      </Router>
-    )
-}
+const AppRouter = ({ isLoggedIn }) => {
+  return (
+    <Router>
+      {isLoggedIn && <Navigation />}
+      <Switch>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+          </>
+        ) : (
+          <Route exact path="/">
+            <Auth />
+          </Route>
+        )}
+      </Switch>
+    </Router>
+  );
+};
 
 export default AppRouter;
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,17 @@
-import React from 'react';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { authService } from "fBase";
 
-export default () => <span>profile</span>
+export default () => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+
+  return (
+    <>
+      <button onClick={onLogOutClick}>logout</button>
+    </>
+  );
+};
