8212d29873d633d399ea8b5a7118d91645f17d0d
yeomkyeorae <duarufp06@naver.com>
2020-12-18 22:08:11 +0900

    get my own nweets

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,7 +3,7 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 import Navigation from "./Navigation";
-import Profile from "routes/Profile";
+import Profile from "../routes/Profile";
 
 const AppRouter = ({ isLoggedIn, userObj }) => {
   return (
@@ -16,7 +16,7 @@ const AppRouter = ({ isLoggedIn, userObj }) => {
               <Home userObj={userObj} />
             </Route>
             <Route exact path="/profile">
-              <Profile />
+              <Profile userObj={userObj} />
             </Route>
           </>
         ) : (
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import { authService } from "fBase";
+import { authService, dbService } from "fBase";
 
-export default () => {
+const Profile = ({ userObj }) => {
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
+  const getMyNweets = async () => {
+    const nweets = await dbService
+      .collection("nweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt")
+      .get();
+    console.log(nweets.docs.map(doc => doc.data()));
+  };
+  useEffect(() => {
+    getMyNweets();
+  }, []);
 
   return (
     <>
@@ -15,3 +26,5 @@ export default () => {
     </>
   );
 };
+
+export default Profile;
